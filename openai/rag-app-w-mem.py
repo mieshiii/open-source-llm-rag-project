b7106efbaeed3bfd7949f1361ce2fa55d26732dd
mieshiii <mieseliel.delacruz@gmail.com>
2024-03-04 18:17:05 +0800

    #15 - add memory context to RAG, delete test ipynb

--- openai/rag-app-w-mem.py
+++ openai/rag-app-w-mem.py
@@ -9,40 +9,47 @@ from langchain_community.vectorstores import FAISS
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain.chains import create_history_aware_retriever
+from langchain_core.messages import HumanMessage, AIMessage
 
 #fetch oai_key
 load_dotenv()
 os.environ["OPENAI_API_KEY"] = os.getenv("oai_key")
 
-st.title('Quickstart App')
+st.title('Retrieval Chain App')
 
 def load_index_data():
   #load data
   loader = PyPDFLoader("../data/MSFT_2022_Annual_Report.pdf")
   pages = loader.load_and_split()
-  #index data into vector store
-  faiss_index = FAISS.from_documents(pages, OpenAIEmbeddings())
-
-  return faiss_index
+ 
+  return pages
 
 def gen_response(input_text):
   llm = OpenAI(temperature=0.9, openai_api_key=os.environ["OPENAI_API_KEY"])
+  
+  #index data into vector store
+  faiss_index = FAISS.from_documents(load_index_data(), OpenAIEmbeddings())
 
-  prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+  retriever = faiss_index.as_retriever()
 
-  <context>
-  {context}
-  </context>
-
-  Question: {input_text}""")
+  prompt = ChatPromptTemplate.from_messages([
+    ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("user", "{input}"),
+  ])
 
   document_chain = create_stuff_documents_chain(llm, prompt)
-
-  retriever = load_index_data().as_retriever()
-  retrieval_chain = create_retrieval_chain(retriever, document_chain)
-
-  res = retrieval_chain.invoke({"input_text": input_text})
-  st.info(res)
+  retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+  retrieval_chain = create_retrieval_chain(retriever_chain, document_chain)
+  
+  #add chat history for context
+  #chat history is out of context for the actual application
+  chat_history = [HumanMessage(content="Can LangSmith help test my LLM applications?"), AIMessage(content="Yes!")]
+  res = retrieval_chain.invoke({
+    "chat_history": chat_history,
+    "input": input_text
+  })
+  st.info(res["answer"])
 
 with st.form('test_form'):
   text = st.text_area("Enter text:")
