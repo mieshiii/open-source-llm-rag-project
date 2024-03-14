8ee801eec12366906fbffc50aff9ecc4641d1bdd
mieshiii <mieseliel.delacruz@gmail.com>
2024-03-14 23:55:35 +0800

    #26 - add base app rag code

---
+++ ollama/app.py
@@ -0,0 +1,50 @@
+import os
+import streamlit as st
+import json
+import operator
+from typing import Annotated, Sequence, TypedDict
+from dotenv import load_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import GPT4AllEmbeddings
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+from langchain.prompts import PromptTemplate
+from langchain_community.chat_models import ChatOllama
+from langchain import hub
+from langchain.schema import Document
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.messages import BaseMessage
+
+#base env variables
+llm = "mistral:instruct"
+
+
+#class for data typing
+class GraphState(TypedDict):
+    """
+    Represents the state of our graph.
+
+    Attributes:
+        keys: A dictionary where each key is a string.
+    """
+
+    keys: Dict[str, any]
+
+def retrieve(state):
+    """
+    Retrieve documents
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        state (dict): New key added to state, documents, that contains retrieved documents
+    """
+    print("---RETRIEVE---")
+    state_dict = state["keys"]
+    question = state_dict["question"]
+    local = state_dict["local"]
+    documents = retriever.get_relevant_documents(question)
+    return {"keys": {"documents": documents, "local": local, "question": question}}
